ab7ee4d75b638c9a2af5f4a77d60bd4952047e2e
Savaş Enes ERATEŞLİ <76652961+savaseeratesli@users.noreply.github.com>
2025-10-29 21:10:49 +0300

    Create install-docker.sh
    CentOS 9 - Install Docker Engine &  Docker Compose & Docker Swarm on CentOS

---
+++ install-docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# CentOS 9 - Install Docker Engine &  Docker Compose & Docker Swarm on CentOS
+# Author: Savaş Enes Erateşli
+# Purpose: Docker container ve diğer araçlarının kurulum scripti.
+# --------------------------------------
